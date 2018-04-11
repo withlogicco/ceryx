31135bbe9dc9ebd67a85b24ba90633016bd8d6fa
Paris Kasidiaris <paris@sourcelair.com>
2018-04-11 18:30:34 +0300

    Remove useless files

--- api/server.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-"""
-Executable for the Ceryx server.
-"""
-
-if __name__ == '__main__':
-    from ceryx import settings
-    from ceryx.api import app
-    app.run(host=settings.API_BIND_HOST, port=settings.API_BIND_PORT)
