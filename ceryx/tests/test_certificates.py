1775ebaba1164bac34b1efa9ec2d35ed40f4cef9
Paris Kasidiaris <paris@sourcelair.com>
2019-04-24 19:22:32 +0300

    Extract certificate creation to utils.

--- ceryx/tests/test_certificates.py
+++ ceryx/tests/test_certificates.py
@@ -7,11 +7,9 @@ import redis
 import requests
 
 from client import CeryxTestClient
+from utils import create_certificates_for_host
 
 
-ALL_CAN_READ = stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
-
-CERTIFICATE_ROOT = "/usr/local/share/certificates"
 CERYX_HOST = "ceryx"
 
 redis_client = redis.Redis(host='redis')
@@ -23,34 +21,16 @@ def test_custom_certificate():
     Ensure that Ceryx uses the given certificate for each route, if configured
     so.
     """
-    certificate_id = uuid.uuid4()
-    certificate_path = f"{CERTIFICATE_ROOT}/{certificate_id}.crt"
-    key_path = f"{CERTIFICATE_ROOT}/{certificate_id}.key"
-    hostname = "custom-certificate.ceryx.test"
-
-    command = [
-        "openssl",
-        "req", "-x509",
-        "-newkey", "rsa:4096",
-        "-keyout", key_path,
-        "-out", certificate_path,
-        "-days", "1",
-        "-subj", f"/C=GR/ST=Attica/L=Athens/O=SourceLair/OU=Org/CN={hostname}",
-        "-nodes",
-    ]
-    subprocess.run(
-        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True,
-    )
-    os.chmod(certificate_path, ALL_CAN_READ)
-    os.chmod(key_path, ALL_CAN_READ)
+    host = f"{uuid.uuid4()}.ceryx.test"
+    certificate_path , key_path = create_certificates_for_host(host)
 
     api_base_url = "http://api:5555/"
 
-    route_redis_key = f"ceryx:routes:{hostname}"
+    route_redis_key = f"ceryx:routes:{host}"
     redis_client.set(route_redis_key, api_base_url)
 
-    settings_redis_key = f"ceryx:settings:{hostname}"
+    settings_redis_key = f"ceryx:settings:{host}"
     redis_client.hset(settings_redis_key, "certificate_path", certificate_path)
     redis_client.hset(settings_redis_key, "key_path", key_path)
 
-    test_client.get(f"https://{hostname}/", verify=certificate_path)
+    test_client.get(f"https://{host}/", verify=certificate_path)
