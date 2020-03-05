1d159992b6d6e44641a7774a95f48da4d3778d9f
Alexander Wellbrock <a.wellbrock@mailbox.org>
2020-03-05 18:27:01 +0100

    Fix responder error on missing default route path
    In newer versions or some environments it may occur that the responder
    API complains about a missing, explicit default route key.

--- api/api.py
+++ api/api.py
@@ -8,7 +8,7 @@ api = responder.API()
 client = RedisClient.from_config()
 
 
-@api.route(default=True)
+@api.route("/", default=True)
 def default(req, resp):
     if not req.url.path.endswith("/"):
         api.redirect(resp, f"{req.url.path}/")
